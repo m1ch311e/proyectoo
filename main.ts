34386241b088dfa32701937dfa6ab44afc6b88f8
m1ch311e <91332142+m1ch311e@users.noreply.github.com>
2021-09-27 16:28:24 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
-function feliz () {
-    let Aire = 0
-    let Luz = 0
-    if (true) {
-        basic.showIcon(IconNames.Asleep)
-        basic.showString("¿tengo AGUA?")
-    } else if (Luz) {
-        basic.showIcon(IconNames.Meh)
-        basic.showString("¿tengo LUZ?")
-    } else if (Aire) {
-        basic.showIcon(IconNames.Tortoise)
-        basic.showString("¿tengo AIRE?")
-    }
+if (true) {
+    basic.showIcon(IconNames.Asleep)
+    basic.showString("¿tengo AGUA?")
+    basic.pause(500)
+    basic.showIcon(IconNames.Meh)
+    basic.showString("¿tengo LUZ?")
+    basic.pause(500)
+    basic.showIcon(IconNames.Tortoise)
+    basic.showString("¿tengo AIRE?")
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
     basic.showLeds(`
         . # . # .
         . # . # .
@@ -19,7 +25,15 @@ function feliz () {
         . # # # .
         `)
     basic.showIcon(IconNames.Happy)
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
 }
-basic.forever(function () {
-	
-})
